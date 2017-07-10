95eb33c85b1b3780b5b82c963dee197a138c5315
Elimrtnz <elimartineztwenty5@gmail.com>
2017-07-10 11:42:59 -0700

    Import intro example

--- app/index.js
+++ app/index.js
@@ -1,1 +1,26 @@
 console.log('App working: open app/index.js to start exploring ES6');
+
+//const num1 = 5;  also can be exported imported
+//const num2 = 10;
+
+//import {add, subtract, multiply, divide} from './math';  OR...
+
+import * as math from './math';
+const { num1, num2} = math;
+const { add } = math;
+
+import $ from 'jquery';
+
+// console.log(`The subtraction total of ${num1} and ${num2} is ${subtract(num1,num2)} `);
+//
+// console.log(`The addition total of ${num1} and ${num2} is ${add(num1,num2)} `);
+//
+// console.log(`The total of ${num1} divided by ${num2} is ${divide(num1,num2)} `);
+//
+// console.log(`The total of ${num1} multiplied by ${num2} is ${multiply(num1,num2)} `);
+
+console.log(`The total of ${num1} multiplied by ${num2} is ${math.multiply(num1,num2)} `);  // math becomes an object
+
+console.log(`The addition total of ${num1}and ${num2} is ${add(num1,num2)} `);
+
+$('#root').append('<h1> I AM APPENDED FROM INDEX .JS </h1>')
